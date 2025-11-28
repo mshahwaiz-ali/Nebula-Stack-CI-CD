bb411eaa4737b90daa766a7aef0a2ddd32e079c5
Ali_Shah <alishahwhaiz96@gmail.com>
2025-11-28 15:24:12 +0500

    test_app.py path fixed

--- api/test/test_app.py
+++ api/test/test_app.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# add api folder to python path |||| will also force python to load api/app.py as module
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
+
+
 from app import app
 
 def test_health_route():
